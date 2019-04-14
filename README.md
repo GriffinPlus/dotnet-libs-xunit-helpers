905baf27a2e033d343dc7ad19826e5a9ba40be9c
Sascha Falk <sascha@falk-online.eu>
2019-04-14 10:49:45 +0200

    Added first version.

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# dotnet-libs-xunit-helpers
-Functionality that comes in handy when writing unit tests using xUnit (.NET Standard 2.0)
+# Griffin+ Helpers for xUnit Tests
+
+[![Build (master)](https://img.shields.io/appveyor/ci/ravenpride/dotnet-libs-xunit-helpers/master.svg?logo=appveyor)](https://ci.appveyor.com/project/ravenpride/dotnet-libs-xunit-helpers/branch/master)
+[![NuGet Version](https://img.shields.io/nuget/v/GriffinPlus.Lib.XunitHelpers.svg)](https://www.nuget.org/packages/GriffinPlus.Lib.XUnitHelpers)
+[![NuGet Downloads](https://img.shields.io/nuget/dt/GriffinPlus.Lib.XUnitHelpers.svg)](https://www.nuget.org/packages/GriffinPlus.Lib.XUnitHelpers)
+
+## Overview
+
+This library contains some useful unit testing functionality is missing in *xUnit*.
+
+## Supported Platforms
+
+The library is entirely written in C# using .NET Standard 2.0.
+
+Therefore it should work on the following platforms (or higher):
+- .NET Framework 4.6.1
+- .NET Core 2.0
+- Mono 5.4
+- Xamarin iOS 10.14
+- Xamarin Mac 3.8
+- Xamarin Android 8.0
+- Universal Windows Platform (UWP) 10.0.16299
---
+++ src/GriffinPlus.Lib.XUnitHelpers/AsyncAssert.cs
@@ -0,0 +1,54 @@
+﻿///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// This file is part of the Griffin+ common library suite (https://github.com/GriffinPlus/dotnet-libs-xunit-helpers)
+//
+// Copyright 2019 Sascha Falk <sascha@falk-online.eu>
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance
+// with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+// on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for
+// the specific language governing permissions and limitations under the License.
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+using System;
+using System.Runtime.ExceptionServices;
+using System.Threading.Tasks;
+
+namespace GriffinPlus.Lib.Tests
+{
+	/// <summary>
+	/// Helper functions for unit tests covering asynchronous code.
+	/// </summary>
+	public class AsyncAssert
+	{
+		/// <summary>
+		/// Ensures that the specified task does not complete within the specified time.
+		/// Raises a <see cref="Exception"/>, if the task completes before the specified timeout.
+		/// </summary>
+		/// <param name="task">The task to observe.</param>
+		/// <param name="timeout">Time to wait for the task to complete (in ms).</param>
+		public static async Task DoesNotCompleteAsync(Task task, int timeout = 500)
+		{
+			// wait for the task to complete or the timeout to elapse
+			var completedTask = await Task.WhenAny(task, Task.Delay(timeout)).ConfigureAwait(false);
+			
+			// abort, if the specified task completed before the timeout task
+			if (completedTask == task) {
+				throw new Exception("Task completed unexpectedly.");
+			}
+
+			// if the task didn't complete, attach a continuation that will raise an exception
+			// on a random thread pool thread, if it ever does complete.
+			try
+			{
+				throw new Exception("Task completed unexpectedly.");
+			}
+			catch (Exception ex)
+			{
+				var info = ExceptionDispatchInfo.Capture(ex);
+				var __ = task.ContinueWith(_ => info.Throw(), TaskScheduler.Default);
+			}
+		}
+	}
+}
